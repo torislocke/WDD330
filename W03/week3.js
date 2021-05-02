a29a50c9b493f01b77a3a113d935a104f3008127
tslocke <tori.bosler@hotmail.com>
2021-05-02 10:07:15 -0700

    Completed Wes Bos Clock activity and updated index

--- W03/week3.js
+++ W03/week3.js
@@ -1,7 +1,13 @@
 /* foreach array method
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach*/
 
-const projects = ['<a href="objectMethods.html">Objects</a>', '<a href="notesW03.html">Week Three Notes</a>'];
+const projects = [
+	'<a href="objectMethods.html">Objects</a>',
+	'<a href="notesW03.html">Week Three Notes</a>',
+	'<a href="teamActivityPartOne.html">Team Activity Part One (Web Bos)</a>',
+	'<a href="teamActivityPartTwo.html">Team Activity Part Two (Web Bos)</a>',
+	'<a href="./practice/digitalClock.html">Wes Bos Digital Clock</a>',
+];
 
 const article = document.querySelector('article');
 let projectList = document.createElement('ul');
