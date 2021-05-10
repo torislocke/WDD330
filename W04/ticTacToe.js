19219919a257e43c053b0b175b941d9538665e24
tslocke <tori.bosler@hotmail.com>
2021-05-10 09:38:46 -0700

    Week 4 Team Project Tic Tac Toe final Draft

--- W04/ticTacToe.js
+++ W04/ticTacToe.js
@@ -1,10 +1,18 @@
-const X_PLAYER = 'x';
-const O_PLAYER = 'circle';
-const restartButton = document.getElementById('restartButton');
+//  ------ retrieve HTML elements ---------
+const board = document.getElementById('board');
+const cellElements = document.querySelectorAll('[cell-test]');
+const resetGame = document.getElementById('reset');
+const restartGame = document.getElementById('restartGame');
+const winningMessageTextElement = document.getElementById('winningText');
+const winningMessageElement = document.getElementById('winningMessage');
 
-restartButton.addEventListener('click', startGame);
+//  ------- Build the game variables------
+const playerX = 'x';
+const playerO = 'circle';
+let circleTurn;
+
 // create an array's within an array to hold all the combinations that win
-const WINNING_COMBINATIONS = [
+const combinationsToWin = [
 	// horizontal combinations
 	[0, 1, 2],
 	[3, 4, 5],
@@ -17,50 +25,56 @@ const WINNING_COMBINATIONS = [
 	[0, 4, 8],
 	[2, 4, 6],
 ];
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
-const board = document.getElementById('board');
-const winningMessageElement = document.getElementById('winningMessage');
-const cellElements = document.querySelectorAll('[data-cell]');
-let circleTurn;
+//  ------------ Event Listeners -----------------
+// clear the board when Reset Game button is clicked
+resetGame.addEventListener('click', startGame);
+// clear the board when Start Game button is clicked
+restartGame.addEventListener('click', startGame);
 
+// use touchend with event listener to enable touch screen and mouse --- need to test this!!!!!!
 cellElements.forEach((cell) => {
-	cell.addEventListener('click', handleClick, { once: true });
+	cell.addEventListener('touchend', handleClick, { once: true });
 });
 
+// -- Functions are hoisted may be placed after the call back ----
 // call the start game function
 startGame();
 
 function startGame() {
 	circleTurn = false;
 	cellElements.forEach((cell) => {
-		cell.classList.remove(X_PLAYER);
-		cell.classList.remove(O_PLAYER);
+		cell.classList.remove(playerX);
+		cell.classList.remove(playerO);
 		cell.removeEventListener('click', handleClick);
 		cell.addEventListener('click', handleClick, {
 			once: true,
 		});
+		// callback the setBoardHoverClass function
 		setBoardHoverClass();
 		winningMessageElement.classList.remove('show');
 	});
 }
-
+// e = event this function looks at grid cell and places either an x or o based upon the current turn
 function handleClick(e) {
 	const cell = e.target;
-	const currentClass = circleTurn ? O_PLAYER : X_PLAYER;
+	// establish the currentClass as either an x or 0
+	const currentClass = circleTurn ? playerO : playerX;
+	// places either an x or o in the cell
 	placeMark(cell, currentClass);
 	// continue to check if a x or o has won
 	if (checkWin(currentClass)) {
-		// if there is a winner trigger the endGame Function
+		// if there is a winner call the endGame Function
 		endGame(false);
 	} else if (isDraw()) {
 		endGame(true);
 	} else {
-		// call the swap turns function to swap turns between x and o
+		// call the swapTurns function to swap between x and o
 		swapTurns();
-		// call the hover class after the swapTurns to show x or circle based on current characters turn
+		// call the hover class after the swapTurns to show x or o based on current characters turn
 		setBoardHoverClass();
 	}
 }
+// ----------- function to print screen message if game is draw
 function endGame(draw) {
 	if (draw) {
 		winningMessageTextElement.innerText = 'Draw!';
@@ -70,37 +84,37 @@ function endGame(draw) {
 	}
 	winningMessageElement.classList.add('show');
 }
-
+// --- using a spread operator ... to look through cellElements array and count (every) cell to determin if game is a draw --- //
 function isDraw() {
 	return [...cellElements].every((cell) => {
-		return cell.classList.contains(X_PLAYER) || cell.classList.contains(O_PLAYER);
+		return cell.classList.contains(playerX) || cell.classList.contains(playerO);
 	});
 }
-
+//  function to look at grid cell and place current character
 function placeMark(cell, currentClass) {
 	cell.classList.add(currentClass);
 }
 
-//create the swap turns function - functions are hoisted so may be placed after the call - simple function toggles between circle and x
+//create the swap turns function -  - simple function toggles between circle and x
 
 function swapTurns() {
 	circleTurn = !circleTurn;
 }
 
-// create the hover state for the board
+// create the hover state for the board callback in start game
 function setBoardHoverClass() {
-	board.classList.remove(X_PLAYER);
-	board.classList.remove(O_PLAYER);
+	board.classList.remove(playerX);
+	board.classList.remove(playerO);
 	if (circleTurn) {
-		board.classList.add(O_PLAYER);
+		board.classList.add(playerO);
 	} else {
-		board.classList.add(X_PLAYER);
+		board.classList.add(playerX);
 	}
 }
 
 function checkWin(currentClass) {
 	/// check if winning combination
-	return WINNING_COMBINATIONS.some((combination) => {
+	return combinationsToWin.some((combination) => {
 		return combination.every((index) => {
 			// check that every element has either an x or o
 			// check each cell and its class
