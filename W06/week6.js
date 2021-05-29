6d9e60ec16d13c350b43c3892510011bb1bb7331
tslocke <tori.bosler@hotmail.com>
2021-05-29 05:23:32 -0700

    update style

--- W06/week6.js
+++ W06/week6.js
@@ -1,7 +1,7 @@
 /* foreach array method
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach*/
 
-const projects = ['<a href="./anothertoDo/index.html">To Do List</a>'];
+const projects = ['<a href="./anotherToDo/index.html">To Do List</a>'];
 
 const article = document.querySelector('article');
 let projectList = document.createElement('ul');
