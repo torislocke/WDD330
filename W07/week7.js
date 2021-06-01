cdff17cd740fc67d20a061a931f474538e7be2a6
tslocke <tori.bosler@hotmail.com>
2021-06-01 08:44:47 -0700

    week 7 notes start

---
+++ W07/week7.js
@@ -0,0 +1,20 @@
+/* foreach array method
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach*/
+
+const projects = [
+	'<a href="./hiking/hiking.html">Team Project | Hikes</a>',
+	'<a href="./hiking/hikes.html">My Version | Hikes</a>',
+	'<a href="notesW07.html">Week Seven Notes</a>',
+	'<a href="../ExtraLearning/piano/">Extra Learning / Drum Kit - Wes Bos</a>',
+	'<a href="../ExtraLearning/imageGallery/">Extra Learning / Image Gallery - Wes Bos</a>',
+];
+
+const article = document.querySelector('article');
+let projectList = document.createElement('ul');
+
+projects.forEach((item) => {
+	let listItem = document.createElement('li');
+	listItem.innerHTML = item;
+	projectList.append(listItem);
+});
+article.append(projectList);
